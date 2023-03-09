0ab00aa2abddfe1667d574c65b3ab873206b399d
rahmanabdulquadir <rahmanaq777@gmail.comm>
2023-03-09 20:01:38 +0600

    images added

--- src/pages/shared/RightSideNav/RightSideNav.js
+++ src/pages/shared/RightSideNav/RightSideNav.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import {
+  FaGoogle,
+  FaGithub,
+  FaTwitter,
+  FaTwitch,
+  FaWhatsapp,
+  FaInstagram,
+  FaFacebook,
+} from "react-icons/fa";
+import ListGroup from "react-bootstrap/ListGroup";
+import BrandCarousal from "../BrandCarousal/BrandCarousal";
 
 const RightSideNav = () => {
   return (
     <div>
-      right side nav
+      <ButtonGroup vertical>
+        <Button className="mb-2" variant="outline-primary">
+          <FaGoogle /> Login with Google
+        </Button>
+        <Button variant="outline-dark">
+          <FaGithub /> Login with GitHub
+        </Button>
+      </ButtonGroup>
+
+      <div className="mt-4">
+        <h5>Find us on</h5>
+        <ListGroup>
+          <ListGroup.Item><FaFacebook/> Facebook</ListGroup.Item>
+          <ListGroup.Item><FaInstagram/> Instagram</ListGroup.Item>
+          <ListGroup.Item><FaWhatsapp/> WhatsApp</ListGroup.Item>
+          <ListGroup.Item><FaTwitter/> Twitter</ListGroup.Item>
+          <ListGroup.Item><FaTwitch/> Twitch</ListGroup.Item>
+        </ListGroup>
+      </div>
+
+      <div>
+        <BrandCarousal></BrandCarousal>
+      </div>
     </div>
   );
 };
